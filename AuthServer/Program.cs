39d0f67cd749aa7b56360561fd201baad080a743
HanDeCheng <Han.De.Cheng@ibm.com>
2023-11-30 16:04:28 +0800

    feat:新增資料庫連線資訊、註冊OpenIddict服務

--- AuthServer/Program.cs
+++ AuthServer/Program.cs
@@ -1,7 +1,50 @@
+using AuthServer.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+    options.UseOpenIddict();
+});
+
+//OpenIddict
+builder.Services.AddOpenIddict()
+    .AddCore(options =>
+    {
+        options.UseEntityFrameworkCore()
+            .UseDbContext<AppDbContext>();
+    })
+    .AddServer(options =>
+    {
+        // 允許 auth server 支援 client credentials grant
+        options.AllowClientCredentialsFlow();
+
+        options
+            // 設定取得 access token 的 endpoint
+            .SetTokenEndpointUris("/connect/token")
+            // 設定 introspection endpoint
+            .SetIntrospectionEndpointUris("/connect/introspect")
+            ;
+
+        options
+            // 產生開發用的加密金鑰，production 建議用存在本機的 X.509 certificates
+            .AddEphemeralEncryptionKey()
+            .AddEphemeralSigningKey()
+            // 停用 access token 加密，production 不建議使用
+            .DisableAccessTokenEncryption()
+            ;
+
+        options
+            .UseAspNetCore()
+            .EnableTokenEndpointPassthrough();
+    });
+
 
 var app = builder.Build();
 
