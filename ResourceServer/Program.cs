f221cf056b8738b85ba0bbaf2d7a2cc989019003
HanDeCheng <Han.De.Cheng@ibm.com>
2023-12-01 10:22:55 +0800

    chore:(ResourceServer-Startup)加入OpenIddict validation

--- ResourceServer/Program.cs
+++ ResourceServer/Program.cs
@@ -1,11 +1,39 @@
+using OpenIddict.Validation.AspNetCore;
+var authServerHost = "https://localhost:7225/";
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+// 加入 Authentication，DefaultScheme 設定為 OpenIddict AuthenticationScheme
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultScheme = OpenIddictValidationAspNetCoreDefaults.AuthenticationScheme;
+});
+
+// 加入 OpenIddict validation
+builder.Services.AddOpenIddict()
+    .AddValidation(options =>
+    {
+        // AuthServer 網址
+        options.SetIssuer(authServerHost);
+
+        // 使用前篇設定 introspection 的 clientid/clientsecret
+        options.UseIntrospection()
+            .SetClientId("resource_server")
+            .SetClientSecret("846B62D0-DEF9-4215-A99D-86E6B8DAB342");
+
+        options.UseSystemNetHttp();
+        options.UseAspNetCore();
+    });
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// 加入 Authorization
+builder.Services.AddAuthorization();
 
 var app = builder.Build();
 
@@ -18,6 +46,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
